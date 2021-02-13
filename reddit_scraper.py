e3024272d247565f08c96eb3564dfd40b4a116d2
Luis Lavieri Hernandez <Luis.LavieriHernande@bottomline.com>
2021-02-13 14:39:56 -0500

    cleanup

---
+++ reddit_scraper.py
@@ -0,0 +1,20 @@
+from psaw import PushshiftAPI
+import datetime as dt
+import pandas as pd
+
+api = PushshiftAPI()
+
+start_epoch = int(dt.datetime(2020, 12, 20).timestamp())
+end_epoch = int(dt.datetime(2021, 1, 21).timestamp())
+
+gen = api.search_comments(
+    after=start_epoch,
+    before=end_epoch,
+    subreddit='stellar',
+    filter=['author', 'body', 'created_utc']
+)
+
+df = pd.DataFrame([thing.d_ for thing in gen])
+
+df['created_at'] = df['created_utc'].apply(lambda x: dt.datetime.fromtimestamp(x))
+df.to_csv('reddit_data.csv')
